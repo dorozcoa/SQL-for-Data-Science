143e9d45f2bf800cdb35e8bfe8084d5e5b754c22
dorozcoa <57046057+dorozcoa@users.noreply.github.com>
2020-08-24 21:32:42 -0500

    Se actualiza el orden para cada consulta
    Se actualiza el orden para cada consulta para la semana 4

--- SQL_data_science_quiz_4.sql
+++ SQL_data_science_quiz_4.sql
@@ -4,10 +4,8 @@
 -- along with combining their city and country together. Be sure to make a
 -- space in between these two and make it UPPER CASE.
 
-SELECT CustomerId,
-       FirstName || " " || LastName AS FullName,
-       Address,
-       UPPER(City || " " || Country) AS CityCountry
+SELECT CustomerId, FirstName || " " || LastName AS FullName, Address,
+   UPPER(City || " " || Country) AS CityCountry
 FROM Customers
 
 ------------------------------------------------------------------------------------
@@ -15,11 +13,8 @@ FROM Customers
 -- employee’s first name with the first 2 letters of the employee’s last name. 
 -- Make the new field lower case and pull each individual step to show your work.
 
-SELECT FirstName,
-       LastName,
-       LOWER(SUBSTR(FirstName,1,4)) AS A,
-       LOWER(SUBSTR(LastName,1,2)) AS B,
-       LOWER(SUBSTR(FirstName,1,4)) || LOWER(SUBSTR(LastName,1,2)) AS userId
+SELECT FirstName,LastName,LOWER(SUBSTR(FirstName,1,4)) AS A,
+       LOWER(SUBSTR(LastName,1,2)) AS B,LOWER(SUBSTR(FirstName,1,4)) || LOWER(SUBSTR(LastName,1,2)) AS userId
 FROM Employees
 
 ------------------------------------------------------------------------------------
@@ -30,8 +25,7 @@ SELECT FirstName,
        LastName,
        HireDate,
        (STRFTIME('%Y', 'now') - STRFTIME('%Y', HireDate)) 
-          - (STRFTIME('%m-%d', 'now') < STRFTIME('%m-%d', HireDate)) 
-          AS YearsWorked
+          - (STRFTIME('%m-%d', 'now') < STRFTIME('%m-%d', HireDate)) AS YearsWorked
 FROM Employees
 WHERE YearsWorked >= 15
 ORDER BY LastName ASC
@@ -39,9 +33,12 @@ ORDER BY LastName ASC
 ------------------------------------------------------------------------------------
 -- Q4) Profiling the Customers table, answer the following question.
 
-SELECT COUNT(*)
-FROM Customers
-WHERE [some_column] IS NULL
+/* SELECT COUNT(*) FROM Customers
+WHERE [some_column] IS NULL */
+
+select *
+from Customers c
+where c.Company is null
 
 -- some_column: FirstName, PostalCode, Company, Fax, Phone, Address
 -- Answers: Postal Code, Company, Fax, Phone
@@ -50,8 +47,7 @@ WHERE [some_column] IS NULL
 -- Q5) Find the cities with the most customers and rank in descending order.
 
 SELECT City,
-       COUNT(*)
-FROM Customers
+       COUNT(*) FROM Customers
 GROUP BY City
 ORDER BY COUNT(*) DESC
 
@@ -60,9 +56,7 @@ ORDER BY COUNT(*) DESC
 -- their first and last name while ordering your query in the following order:
 -- firstname, lastname, and invoiceID.
 
-SELECT C.FirstName,
-       C.LastName,
-       I.InvoiceId,
+SELECT C.FirstName,C.LastName,I.InvoiceId,
        C.FirstName || C.LastName || I.InvoiceID AS NewId
 FROM Customers C INNER JOIN Invoices I
 ON C.CustomerId = I.CustomerID
